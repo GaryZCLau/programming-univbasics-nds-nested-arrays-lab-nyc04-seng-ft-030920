d39a037ec3932f52fccb07334892d4d832ef6a62
benign-fork-5056 <gareeeliu@gmail.com>
2020-02-28 07:17:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -25,6 +25,7 @@ def assembled_matrix
  ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
  ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
 ]
+produce
 end
 
 def sorted_matrix
